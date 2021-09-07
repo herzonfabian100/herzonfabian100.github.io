4ce36596c465a514970c8856285c8ec92f0e6950
Hz Fabian Aparicio <herzonfabian100@gmail.com>
2021-09-06 18:19:45 -0700

    Updated Form and Edit

--- js/index.js
+++ js/index.js
@@ -1,91 +1,109 @@
-let today = new Date();
+const today = new Date();
 const ThisYear = today.getFullYear();
 const footer = document.querySelector("footer");
 const copyright = document.createElement("P"); // Create paragraph
+//Create Footer
 copyright.innerHTML = `&copy;Fabian Aparicio  ${ThisYear}`;
 footer.appendChild(copyright);
-
+//Skills 
 const skills = ["Web sides", "JavaScript", "Html", "Internet Conection", "GIT", "GitHub", "Visual Studio"];
 const skillsSection = document.getElementById("skills1");
 const skillsList = skillsSection.querySelector("ul");
+// Get Form with DOM
+const messageForm = document.querySelector('form[name="leave_message"]');
+// console.log(messageForm);
+const submitButton = document.querySelector('button[type="submit"]');
+// console.log(submit);
+// Messages Form Const
+const messageSection = document.querySelector('#messages');
+const messageList = messageSection.querySelector('ul');
+
+
 //Show up skill
 for (let i = 0; i < skills.length; i++) {
     var skill = document.createElement("li");
     skill.innerHTML = `${skills[i]}`;
     skillsList.appendChild(skill);
 }
-// Get Form with DOM
-const messageForm = document.querySelector('form[name="leave_message"]');
-console.log(messageForm);
-const submit = document.querySelector('button[type="submit"]');
-console.log(submit);
 
 
+function createMessagesList(name, email, textarea) {
+    const newMessage = document.createElement('li');
+    const removeButton = document.createElement('button');
+    const editButton = document.createElement('button');
+    // newMessage.innerHTML = `<div>
+    //     <span class="message strong">${textarea.value}</span>
+    //     <p>${today.toLocaleString()} from <a class="link" href="mailto:${
+    //     email.value
+    //   }">${name.value}</a> &nbsp;</p>
+    //   </div>`;
 
-function HideMessages() {
-    document.getElementById("messages").style.visibility = "hidden";
+    newMessage.innerHTML = ` From: <a href="mailto:${email.value}">${name.value} </a> <br> <span class="message">  Message: ${textarea.value}</span> 
+    <p>${today.toLocaleString()} </p>
+    `;
+    //Remove Button
+    removeButton.innerText = 'Remove';
+    removeButton.setAttribute('type', 'button');
+    //Edit Button
+    editButton.innerText = 'Edit';
+    editButton.setAttribute('type', 'button');
+    //Appending
+    newMessage.appendChild(editButton);
+    newMessage.appendChild(removeButton);
+    //Create message 
+    messageList.appendChild(newMessage);
+    //Display Messages Section
+    messageSection.style.display = '';
+
 }
-HideMessages();
 
-function ShowMessages() {
-    document.getElementById("messages").style.visibility = "visible";
-
-}
-
-
-
-
-function ResetForm() {
-    document.getElementById("Form_Message").reset();
-}
-
-submit.addEventListener('click', ButtonElements);
-
-
-
-function ButtonElements(event) {
+//Form Value Collection
+submitButton.addEventListener('click', (event) => {
     event.preventDefault();
-    ShowMessages();
-
     const name = document.querySelector('input[name="name"]');
     const email = document.querySelector('input[name="email"]');
     const textarea = document.querySelector('textarea[name="message"]');
-    console.log(name.value);
-    console.log(email.value);
-    console.log(textarea.value);
-    const messageSection = document.querySelector('#messages');
-    const messageList = messageSection.querySelector('ul');
-    const newMessage = document.createElement('li');
+    createMessagesList(name, email, textarea);
+    messageForm.reset();
+});
 
 
-
-
-    //Create message 
-    newMessage.innerHTML = `<div>
-        <span class="strong">${textarea.value}</span>
-        <p>${today.toLocaleString()} from <a class="link" href="mailto:${
-        email.value
-      }">${name.value}</a> &nbsp;</p>
-      </div>`;
-    messageList.appendChild(newMessage);
-
-    //Create BUTTON
-
-    const removeButton = document.createElement('BUTTON');
-    removeButton.innerText = 'remove';
-    removeButton.setAttribute('type', 'button');
-    newMessage.appendChild(removeButton);
-    removeButton.addEventListener('click', deleteItem)
-
-    function deleteItem(e) {
-        const entry = e.target.parentNode;
-        entry.remove();
-        console.log(entry);
-
-    }
-
-    ResetForm();
+//Hide Messages Section if Empty
+if (messageList.children.length == 0) {
+    messageSection.style.display = 'none';
 }
+
+messageList.addEventListener('click', (e) => {
+    if (e.target.tagName === 'BUTTON') {
+        const button = e.target;
+        const li = button.parentNode;
+        const ul = li.parentNode;
+        if (button.textContent === 'Remove') {
+            ul.removeChild(li);
+            if (messageList.children.length == 0) {
+                messageSection.style.display = 'none';
+            }
+        } else if (button.textContent === 'Edit') {
+            const span = li.querySelector('span');
+            console.log(span);
+            const input = document.createElement('input');
+            input.className += 'input';
+            input.type = 'text';
+            input.value = span.textContent;
+            li.insertBefore(input, span);
+            li.removeChild(span);
+            button.textContent = 'Save';
+        } else if (button.textContent === 'Save') {
+            const input = li.querySelector('input');
+            const span = document.createElement('span');
+            span.textContent = input.value;
+            li.insertBefore(span, input);
+            li.removeChild(input);
+            button.textContent = 'Edit';
+        }
+    }
+});
+
 //Display Projects in GitHub with Fetch
 fetchData('https://api.github.com/users/herzonfabian100/repos')
 
